8b7f7c06ac4d413c45522d350a3e9448a6a77ac1
Mukund Akabari <akabarimukund36@gmail.com>
2025-02-07 22:32:16 +0530

    Delete src/main/java/com/example/sale_campaign_system/Repository/CDRepository.java

--- src/main/java/com/example/sale_campaign_system/Repository/CDRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sale_campaign_system.Repository;
-
-import com.example.sale_campaign_system.Model.CampaignDiscount;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CDRepository extends JpaRepository<CampaignDiscount,Integer> {
-}
